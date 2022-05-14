c8f1ac7d0841ca044d7e255ce938eb7291060e78
Samuel Gonzalez <samglz10@gmail.com>
2022-05-14 12:17:44 -0400

    added a Restart Button

---
+++ js/RestartButton.js
@@ -0,0 +1,5 @@
+const button = document.querySelector("#restart-button")
+
+button.onclick = () => {
+    window.location.reload();
+}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-let character = newImage("assets/flyexample.jpg")
+let character = newImage("assets/ham.png");
 character.style.position = "absolute"
 let direction = null;
 let x = 100;
