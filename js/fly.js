eada13b37f43ec45fe6c59d386757e28d0ac3cfe
Samuel Gonzalez <samglz10@gmail.com>
2022-05-16 21:36:18 -0400

    adding functions

--- js/fly.js
+++ js/fly.js
@@ -1,6 +1,24 @@
+
+// fly needs to reach bottom value to reach the meat
+
+// Function send Fly to Meat, the clear the interval
+//setInterval to set slowly change the y, throw math.random on x value
+newFly (x,y) => {
+    let element = newFly ('./assets/flyicon.png')
+    element.style.zIndex = 1;
+
+    let direction = null;
+
+    moveFly() => {
+        if ( direction =)
+
+    
+    }
+
+}
+
+// Check if Fly postion is at the meat if so clear interval. 
 const fly = document.createElement('img');
     fly.src= "./assets/flyicon.png";
     document.getElementById("browser-window").appendChild(fly);
     fly.classList.add("fly");
-    
-
--- js/main.js
+++ js/main.js
@@ -2,4 +2,3 @@ let hpBar = document.createElement('img');
     hpBar.src= "./assets/healthcarrion.png";
     document.getElementById("browser-window").appendChild(hpBar);
     hpBar.classList.add("hp-bar");
-
