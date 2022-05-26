b1610242290fdc58c24e371e2d273abe919f434d
Samuel Gonzalez <samglz10@gmail.com>
2022-05-25 20:26:09 -0400

    scoreboard loop

--- js/RestartButton.js
+++ js/RestartButton.js
@@ -10,6 +10,7 @@ document.getElementById("cursor").style.cursor = "crosshair";
 const myScore = () => {
 	let scoreCounter =0;
 	document.getElementById("scoreboard").textContent = scoreCounter;
+	for (let score =0; score < 20; i++)
 	scoreCounter++;
 
 }
